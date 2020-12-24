2ae096db1f84c1687f7066da93dba55f08d2f8e1
Hassan Aziz <hassanaziz0012@gmail.com>
2020-12-24 20:37:52 +0500

    Added logging

--- game.py
+++ game.py
@@ -3,7 +3,11 @@ in the config.py file."""
 
 import pygame
 from config import *
+import logging
+from datetime import datetime
 
+CURRENT_TIME = str(datetime.now().time().__str__()) # Used mostly in logging messages to log when the log message was made.
+logging.basicConfig(filename=f"logs/{str(datetime.today().date().__str__())}.log", filemode='a', level=logging.DEBUG)
 
 pygame.display.init()
 screen_title = pygame.display.set_caption('Chess')
@@ -13,8 +17,6 @@ class Chessboard:
     moves = ChessNotation.moves
 
     def create_chessboard():
-        # TODO: We should be able to convert each of these into for loops. Remove all the duplicate code.
-        
         """Row is incremented after every loop so that we draw on the next row. The current_square
         is changed from BLACK_SQUARE to WHITE_SQUARE and from WHITE_SQUARE to BLACK_SQUARE again and
         again, because otherwise it would simply create a straight line of black and white squares.
@@ -23,20 +25,23 @@ class Chessboard:
         
         row = 0
         current_square = BLACK_SQUARE
-        for i in range(0, 8):
-            for j in range(0, 8):
-                if current_square == BLACK_SQUARE:
-                    screen.blit(BLACK_SQUARE, (SQUARE_SIZE*j, SQUARE_SIZE*row))
-                    current_square = WHITE_SQUARE
+        try:
+            for i in range(0, 8):
+                for j in range(0, 8):
+                    if current_square == BLACK_SQUARE:
+                        screen.blit(BLACK_SQUARE, (SQUARE_SIZE*j, SQUARE_SIZE*row))
+                        current_square = WHITE_SQUARE
 
-                elif current_square == WHITE_SQUARE:
-                    screen.blit(WHITE_SQUARE, (SQUARE_SIZE*j, SQUARE_SIZE*row))
-                    current_square = BLACK_SQUARE
-            row += 1
-            if i % 2 == 1:
-                current_square = BLACK_SQUARE 
-            elif i % 2 == 0:
-                current_square = WHITE_SQUARE
+                    elif current_square == WHITE_SQUARE:
+                        screen.blit(WHITE_SQUARE, (SQUARE_SIZE*j, SQUARE_SIZE*row))
+                        current_square = BLACK_SQUARE
+                row += 1
+                if i % 2 == 1:
+                    current_square = BLACK_SQUARE 
+                elif i % 2 == 0:
+                    current_square = WHITE_SQUARE
+        except Exception:
+            logging.critical(f'{CURRENT_TIME} - An unexpected error occured while creating the chessboard. The chessboard could not be created.')
             
     def create_chess_pieces():
         # BLACK PIECES
