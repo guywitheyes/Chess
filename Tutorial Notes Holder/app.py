f5e64106587c1f6b581ec238d6d43eb4f1212dca
Hassan Aziz <hassanaziz0012@gmail.com>
2021-01-03 11:27:16 +0500

    ttt

---
+++ Tutorial Notes Holder/app.py
@@ -0,0 +1,5 @@
+import itertools
+
+counter = itertools.cycle([1, 2, 3])
+print(next(counter))
+
---
+++ Tutorial Notes Holder/delete.py
@@ -0,0 +1,5 @@
+import pyautogui, time
+
+time.sleep(1)
+for i in range(0, 100):
+    pyautogui.click()
