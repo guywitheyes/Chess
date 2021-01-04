3b49495d31b713b3160113bb9eb56efa923dfdf9
Hassan Aziz <hassanaziz0012@gmail.com>
2021-01-04 16:06:51 +0500

    Added a MOUSEDOWNEVENT for moving pieces

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 """This file is used to run the game loop."""
-
-
 import pygame
-from game import Chessboard, screen
+from game import Chessboard
+from config import Position, PieceCoordinates
 from sys import exit
 
 running = True
 while running:
     pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    screen.fill('brown')
+        # TODO: Figure out how to select a piece before moving it.
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                x, y = event.pos
+                print(PieceCoordinates.check_piece_coordinates(x, y))
+                # for piece_position in Position.BLACK_PIECES_ALL_POSITIONS:
+
+                    # if PieceCoordinates.check_piece_coordinates(x, y):
+                    #     Position.move_piece(Position.BLACK_KING_POSITION, 'd4')
+
     Chessboard() # Create chessboard.
