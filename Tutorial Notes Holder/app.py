ddf4b65cbc422eda132be46e49ce28fd3f130d3c
Hassan Aziz <hassanaziz0012@gmail.com>
2021-01-04 22:32:48 +0500

    Created Django project

--- Tutorial Notes Holder/app.py
+++ Tutorial Notes Holder/app.py
@@ -1,2 +1,5 @@
-
-
+class PizzaUndeliveredError(Exception):
+    def __init__(self, message):
+        self.message = message
+# raising the error:
+raise PizzaUndeliveredError('The pizza has still not been delivered.')
