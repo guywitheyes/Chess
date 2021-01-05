dd94aa80a3f45f5f2d06cc1d1351fa886bd6e688
Hassan Aziz <hassanaziz0012@gmail.com>
2021-01-05 20:11:19 +0500

    Database and Migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,33 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
-
-posts = [
-    {
-        'author': 'Hassan Aziz',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'August 27, 2019'
-    },
-    {
-        'author': 'Hassan Aziz',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'August 27, 2019'
-    },
-    {
-        'author': 'Hassan Aziz',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'August 27, 2019'
-    }
-]
-
 
 def home(request):
     context = {
         'title': 'Home',
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
